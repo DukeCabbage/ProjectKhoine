803f36dd58fb3b0d4dca6b625c46a5d1520a4545
DukeCabbage <liaowng@gmail.com>
2016-05-23 17:27:54 -0700

    testing request to sourse

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const firebase = require("firebase");
 const domain = "localhost";
 const port = "8080";
 
+const forecast_path = "https://api.forecast.io/forecast/";
+const api_key = "f2778d0372266ff4099d2ee062db20bf";
+
+const home_lat = 49.168625;
+const home_lng = -123.142105;
+
+const SUCCESS = 200;
+const BAD_REQUEST = 400;
+const INTERNAL_ERROR = 500;
+
 var app = express();
 
 app.get("/health-check", (req, res) => {
@@ -13,6 +23,35 @@ app.get("/health-check", (req, res) => {
     res.end();
 });
 
-app.listen(port, domain, () => {
-    console.log("Server started on " + domain + ":" + port);
+app.get("/test", (req, res) => {
+    var lat = req.query.lat;
+    var lng = req.query.lng;
+    if (isNaN(lat) || isNaN(lng)) {
+        res.status(BAD_REQUEST).send("Invalid latlng");
+    } else {
+        console.log("Fetching weather for %s, %s", lat, lng);
+        var requestString = forecast_path + api_key + "/" + lat + "," + lng;
+        requestString += "?exclude=[minutely, hourly, daily, flags]";
+        request(requestString, (error, response, body) => {
+            var defaultErrorStr = "Failed to fetch from forecase.io";
+            if (typeof body === "string") {
+                body = JSON.parse(body);
+            }
+            if (error || response.statusCode >= 400) {
+                console.log(error || body);
+                var code = body == null ? INTERNAL_ERROR : body["code"] || INTERNAL_ERROR;
+                var errorStr = body == null ? defaultErrorStr : body["error"] || defaultErrorStr;
+                res.status(code).send(errorStr);
+            } else {
+                res.status(SUCCESS).send(body);
+            }
+        });
+    }
+});
+
+var server = app.listen(port, domain, () => {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Server started on http://%s:%s", host, port);
 });
